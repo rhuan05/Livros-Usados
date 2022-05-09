154f5fb177f316fed79167796ad78c0c17ab0339
Rhuan Henrique <rhuanhenrique964@gmail.com>
2022-05-09 05:21:37 -0300

    Livros Usados 2.0

--- livros.js
+++ livros.js
@@ -1,4 +1,16 @@
-let livrosJson = [
+let maisPopulares = [
+    {name:'Força de Vontade', img:'./images/forcadevontade_frente.jpeg', price:'R$15,00'},
+    {name:'O Médico e o Monstro', img:'./images/medicoeomonstro_frente.jpeg', price:'R$20,00'},
+    {name:'Esperança Além da Crise', img:'./images/esperancaalemdacrise_frente.jpeg', price:'R$20,00'},
+];
+
+let talvezGoste = [
+    {name:'Força de Vontade', img:'./images/forcadevontade_frente.jpeg', price:'R$15,00'},
+    {name:'O Médico e o Monstro', img:'./images/medicoeomonstro_frente.jpeg', price:'R$20,00'},
+    {name:'Esperança Além da Crise', img:'./images/esperancaalemdacrise_frente.jpeg', price:'R$20,00'},
+];
+
+let outros = [
     {name:'Força de Vontade', img:'./images/forcadevontade_frente.jpeg', price:'R$15,00'},
     {name:'O Médico e o Monstro', img:'./images/medicoeomonstro_frente.jpeg', price:'R$20,00'},
     {name:'Esperança Além da Crise', img:'./images/esperancaalemdacrise_frente.jpeg', price:'R$20,00'},
--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 let titulos = document.querySelectorAll('.titulo');
 let precos = document.querySelectorAll('.precoLivro');
 let modeloLivro = document.querySelector('.fileira--livros--sobre');
+let imgBanner = document.getElementById('imgBanner');
 
-livrosJson.map((e, i) => {
+if (window.screen.width <= 375){
+    imgBanner.src = '/images/imgBanner320px.png';
+} else if (window.screen.width <= 425){
+    imgBanner.src = '/images/imgBanner425px.png';
+} else {
+    imgBanner.src = '/images/banner.png';
+}
+
+maisPopulares.map((e, i) => {
     let novoLivro = modeloLivro.cloneNode(true);
-    document.querySelector('.fileira--livros').appendChild(novoLivro);
-    document.querySelector('.titulo').innerHTML = livrosJson[i].name;
-    document.querySelector('.precoLivro').innerHTML = livrosJson[i].price;
-    document.querySelector('.imgLivro').src = livrosJson[i].img;
+    document.querySelector('#mais-populares .fileira--livros').appendChild(novoLivro);
+    document.querySelector('.titulo').innerHTML = maisPopulares[i].name;
+    document.querySelector('.precoLivro').innerHTML = maisPopulares[i].price;
+    document.querySelector('.imgLivro').src = maisPopulares[i].img;
+});
+
+talvezGoste.map((e, i) => {
+    let novoLivro = modeloLivro.cloneNode(true);
+    document.querySelector('#talvez-goste .fileira--livros').appendChild(novoLivro);
+    document.querySelector('.titulo').innerHTML = talvezGoste[i].name;
+    document.querySelector('.precoLivro').innerHTML = talvezGoste[i].price;
+    document.querySelector('.imgLivro').src = talvezGoste[i].img;
+});
+
+outros.map((e, i) => {
+    let novoLivro = modeloLivro.cloneNode(true);
+    document.querySelector('#outros .fileira--livros').appendChild(novoLivro);
+    document.querySelector('.titulo').innerHTML = outros[i].name;
+    document.querySelector('.precoLivro').innerHTML = outros[i].price;
+    document.querySelector('.imgLivro').src = outros[i].img;
 });
 
 let btnsWpp = document.querySelectorAll('.btnWpp');
@@ -19,7 +44,7 @@ document.addEventListener('click', (e) => {
         let sobreElementos = elementoClick.parentNode;
         let mainEscolhido = document.getElementById('main--escolhido');
         mainEscolhido.style.display = 'block';
-        mainEscolhido.style.top = `${window.scrollY + 50}px`
+        mainEscolhido.style.top = `${window.scrollY}px`
         document.getElementById('titulo--escolhido').innerHTML = sobreElementos.children[1].innerHTML;
         document.getElementById('preco--escolhido').innerHTML = sobreElementos.children[3].innerHTML;
         document.getElementById('imagem--escolhido').src = sobreElementos.children[0].src;
